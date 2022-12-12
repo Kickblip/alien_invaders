75527010637bf82032e6d9b41a8d0b1d7119d85b
Kickblip <wyatt.zilker@gmail.com>
2022-12-12 11:19:27 -0600

    ship life sprites

--- alien_invaders.py
+++ alien_invaders.py
@@ -144,6 +144,10 @@ class AlienInvaders:
             self.bullets.empty()
             self._create_fleet()
             self.settings.increase_speed()
+
+            # Increase level
+            self.stats.level += 1
+            self.sb.prep_level()
     
     def _create_fleet(self):
         """Create the fleet of aliens"""
@@ -205,8 +209,9 @@ class AlienInvaders:
     def _ship_hit(self):
         """Respond to the ship being hit by an alien"""
         if self.stats.ships_left > 0:
-            # Decrement ships_left
+            # Decrement ships_left, and update scoreboard
             self.stats.ships_left -= 1
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
@@ -245,6 +250,8 @@ class AlienInvaders:
             self.stats.reset_stats()
             self.stats.game_active = True
             self.sb.prep_score()
+            self.sb.prep_level()
+            self.sb.prep_ships()
 
             # Get rid of any remaining aliens and bullets
             self.aliens.empty()
